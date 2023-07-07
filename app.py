ac6d05cf736452e610c2448c96b823f68b5ce526
Iván Cordero <122031478+ivancorderoc@users.noreply.github.com>
2023-07-07 20:42:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
-
+import git 
 app = Flask(__name__)
+
+# Route for the GitHub webhook
+
+@app.route('/git_update', methods=['POST'])
+def git_update():
+    repo = git.Repo('./py.anywhere')
+    origin = repo.remotes.origin
+    repo.create_head('main',
+                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return '', 200
+
 @app.route('/',methods=['GET'])
 def home_page():
     message="Welcome to the Home Page"
