3941fed59ce5f6b3f640bf459a7d9a2e05c7b5a2
Iván Cordero <122031478+ivancorderoc@users.noreply.github.com>
2023-07-07 19:52:16 +0200

    bug fixes

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+@app.route('/',methods=['GET'])
+def home_page():
+    message="Welcome to the Home Page"
+    response={"message":message}
+    return jsonify({"response":response}), 200
+
+if __name__ == '__main__':
+    app.run()
+
