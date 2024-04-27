705f358cbc41bb503479e32cdc4ae5b4af9aa524
ivancordero <122031478+ivancorderoc@users.noreply.github.com>
2024-04-27 12:14:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pickle
 import subprocess
 
 app = Flask(__name__)
-root_path = "/home/sikerbet/sikerbet/model/"
+root_path = "/home/sikerbet/sikerbet.py.anywhere/model/"
 
 # Funcion para cargar el modelo y el encoder desde los archivos pickle
 def load_model_and_encoder():
